264135875b54f69304245742a9913d5c5785ce3d
mehdi <mahdi.k.m.g.6@gmail.com>
2024-12-15 04:05:10 +0330

    tracker syntax bug fixed

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,25 +1,28 @@
-const createTransactionTracker = (web3) {
-    const checkTransaction = async (txHash, { onSuccess, onError }) => {
-        try {
-            const receipt = await web3.eth.getTransactionReceipt(txHash)
-            
-            if (!receipt) {
-                // Transaction not yet mined, check again in 2 seconds
-                setTimeout(() => checkTransaction(txHash, { onSuccess, onError }), 2000)
-                return
-            }
+const createTransactionTracker = (web3) => {
+  const checkTransaction = async (txHash, { onSuccess, onError }) => {
+    try {
+      const receipt = await web3.eth.getTransactionReceipt(txHash);
 
-            if (receipt.status) {
-                onSuccess?.(receipt)
-            } else {
-                onError?.(new Error('Transaction failed'))
-            }
-        } catch (error) {
-            onError?.(error)
-        }
+      if (!receipt) {
+        // Transaction not yet mined, check again in 2 seconds
+        setTimeout(
+          () => checkTransaction(txHash, { onSuccess, onError }),
+          2000
+        );
+        return;
+      }
+
+      if (receipt.status) {
+        onSuccess?.(receipt);
+      } else {
+        onError?.(new Error("Transaction failed"));
+      }
+    } catch (error) {
+      onError?.(error);
     }
+  };
 
-    return checkTransaction
-}
+  return checkTransaction;
+};
 
-export default createTransactionTracker
+export default createTransactionTracker;
